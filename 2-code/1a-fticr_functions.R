ef6fa40b699276cfc9d3ef7caf2d552da03df586
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2026-02-20 10:45:11 -0800

    move fticr_data functions into fticr_functions

--- 2-code/1a-fticr_functions.R
+++ 2-code/1a-fticr_functions.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 theme_set(theme_bw(base_size = 14))
 
+#############################
+#############################
 
 # 1. PROCESSING FUNCTIONS -------------------------------------------------
 ## LEVEL I FUNCTIONS -------------------------------------------------------
@@ -177,6 +179,144 @@ make_fticr_data = function(icr_report, sample_key){
 }
 
 #
+
+
+#############################
+#############################
+
+
+icr_process_metadata = function(icr_report_negative, icr_report_positive){
+  
+  meta_negative = make_fticr_meta(icr_report = icr_report_negative)$meta2
+  meta_positive = make_fticr_meta(icr_report = icr_report_positive)$meta2
+  
+  meta_combined = 
+    meta_negative %>% mutate(mode = "negative") %>% 
+    bind_rows(meta_positive %>% mutate(mode = "positive")) %>% 
+    group_by(formula) %>% 
+    mutate(n = n()) %>% 
+    mutate(mode = case_when(n == 2 ~ "negative, positive", TRUE ~ mode)) %>% 
+    distinct()
+}
+
+icr_process_data_trt = function(icr_report_negative, icr_report_positive, sample_key){
+  
+  data_negative = make_fticr_data(icr_report_negative, sample_key)$data_long_trt
+  data_positive = make_fticr_data(icr_report_positive, sample_key)$data_long_trt
+  
+  icr_data_trt = 
+    data_negative %>% mutate(mode = "negative") %>% 
+    bind_rows(data_positive %>% mutate(mode = "positive"))
+  
+}
+
+icr_process_data_cores = function(icr_report_negative, icr_report_positive, sample_key){
+  
+  data_negative_longform = make_fticr_data(icr_report_negative, sample_key)$data_long_blank_corrected
+  data_positive_longform = make_fticr_data(icr_report_positive, sample_key)$data_long_blank_corrected
+  
+  icr_data_cores = 
+    data_negative_longform %>% mutate(mode = "negative") %>% 
+    bind_rows(data_positive_longform %>% mutate(mode = "positive")) %>% 
+    dplyr::select(-analysis_ID, -n)
+  
+}
+
+compute_icr_pca = function(icr_relabundance, sample_key){
+  
+  fit_pca_function = function(dat){
+    relabund_pca=
+      dat %>% 
+      ungroup %>% 
+      #  dplyr::select(-c(abund, total)) %>% 
+      #  spread(Class, relabund) %>% 
+      #  replace(.,is.na(.),0)  %>% 
+      dplyr::select(-1)
+    
+    num = 
+      relabund_pca %>% 
+      dplyr::select(where(is.numeric))
+    
+    grp = 
+      relabund_pca %>% 
+      dplyr::select(-where(is.numeric)) %>% 
+      dplyr::mutate(row = row_number())
+    
+    pca_int = prcomp(num, scale. = T)
+    
+    list(num = num,
+         grp = grp,
+         pca_int = pca_int)
+  }
+  
+  relabund_wide = 
+    pivot_wider(icr_relabundance %>% dplyr::select(-abund, -total), names_from = "Class", values_from = "relabund") %>% 
+    left_join(sample_key %>% dplyr::select(-analysis_ID)) %>% 
+    filter(horizon != "B")
+  
+  pca_negative = fit_pca_function(relabund_wide %>% filter(mode == "negative"))
+  
+  ## plot PCA
+  ggbiplot(pca_negative$pca_int, obs.scale = 1, var.scale = 1,
+           groups = as.character(pca_negative$grp$transect_location), 
+           ellipse = FALSE, circle = FALSE, 
+           var.axes = TRUE, alpha = 0) +
+    geom_convexhull(aes(group = groups,
+                        fill = groups), 
+                    alpha = 0.2)+
+    geom_point(size = 5,stroke = 1, #alpha = 0.5,
+               aes(shape = as.character(pca_negative$grp$site),
+                   fill = groups))+
+    labs(shape = "Site",
+         title = "Surface horizons only")+
+    scale_shape_manual(values = c(21, 23))+
+    scale_fill_manual(values = pnw_palette("Bay", 3))+
+    xlim(-3, 3)+
+    ylim(-2,2)+
+    guides(fill = guide_legend(override.aes = list(shape = 21)))
+  
+  
+  pca_positive = fit_pca_function(relabund_wide %>% filter(mode == "positive"))
+  
+  ## plot PCA
+  ggbiplot(pca_positive$pca_int, obs.scale = 1, var.scale = 1,
+           groups = as.character(pca_positive$grp$transect_location), 
+           ellipse = FALSE, circle = FALSE, 
+           var.axes = TRUE, alpha = 0) +
+    geom_convexhull(aes(group = groups,
+                        fill = groups), 
+                    alpha = 0.2)+
+    geom_point(size = 5,stroke = 1, #alpha = 0.5,
+               aes(shape = as.character(pca_positive$grp$site),
+                   fill = groups))+
+    labs(shape = "Site",
+         title = "Surface horizons only")+
+    scale_shape_manual(values = c(21, 23))+
+    scale_fill_manual(values = pnw_palette("Bay", 3))+
+    xlim(-3, 3)+
+    ylim(-2,2)+
+    guides(fill = guide_legend(override.aes = list(shape = 21)))
+  
+  
+}
+
+
+
+
+
+
+
+
+
+#############################
+#############################
+
+
+
+
+
+
+
 # 2.  ANALYSIS FUNCTIONS --------------------------------------------------
 
 ## Van Krevelen -----------------------------------------------------------
