9ee72eeb0cae35eb93d916fc7456158b5d7917c3
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2026-03-23 18:54:03 -0700

    add gh4x package

--- 2-code/0-packages.R
+++ 2-code/0-packages.R
@@ -7,6 +7,7 @@ library(PNWColors)
 library(ggConvexHull)
 #devtools::install_github("cmartin/ggConvexHull")
 library(whistledown)
+library(ggh4x)
 
 theme_kp <- function() {  # this for all the elements common across plots
   theme_bw() %+replace%
